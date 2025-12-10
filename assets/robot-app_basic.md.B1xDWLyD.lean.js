ec11dc7cb0feb4e553e414441bcfdbd31adaf24c
sw.han <sw.han@wego-robotics.com>
2025-12-10 15:35:15 +0900

    fix for guide

---
+++ assets/robot-app_basic.md.B1xDWLyD.lean.js
@@ -0,0 +1,23 @@
+import{_ as a,c as l,o as e,j as t,ah as s,a as o,bx as r,by as n,bz as d,bA as c,bB as g,bC as p,bD as h,bE as m,bF as u,bG as b,bH as f,bI as y,bJ as x,bK as _,bL as S,bM as v,bN as w,bO as k,bP as P,bQ as C,bR as T}from"./chunks/framework.Bb6kz8E2.js";const j=JSON.parse('{"title":"🤖 Mini Pi 로봇 기본 사용법","description":"","frontmatter":{},"headers":[],"relativePath":"robot-app/basic.md","filePath":"robot-app/basic.md"}'),L={name:"robot-app/basic.md"};function V(R,i,I,q,A,E){return e(),l("div",null,[...i[0]||(i[0]=[t("h1",{id:"🤖-mini-pi-로봇-기본-사용법",tabindex:"-1"},[o("🤖 Mini Pi 로봇 기본 사용법 "),t("a",{class:"header-anchor",href:"#🤖-mini-pi-로봇-기본-사용법","aria-label":"Permalink to “🤖 Mini Pi 로봇 기본 사용법”"},"​")],-1),t("p",null,"Mini Pi의 전원은 3단계로 구성되어 있습니다. 로봇 전원을 켜기 위한 단계 :",-1),t("figure",{class:"vpd-diagram vpd-diagram--mermaid",onclick:`
+        const figure = this;
+        const isFullscreen = figure.classList.contains('vpd-diagram--fullscreen');
+
+        document.querySelectorAll('.vpd-diagram').forEach(diagram => {
+          diagram.classList.remove('vpd-diagram--fullscreen');
+        });
+
+        if (!isFullscreen) {
+          figure.classList.add('vpd-diagram--fullscreen');
+        }
+      `},[t("img",{src:"/minipi_textbook/asset/diagrams/mermaid-basic-6-9d5132c91401e2bb56b1f75a4c9c8ff6.svg",alt:"mermaid Diagram",class:"vpd-diagram-image"})],-1),t("p",null,"로봇 전원을 끄기 위한 단계 :",-1),t("figure",{class:"vpd-diagram vpd-diagram--mermaid",onclick:`
+        const figure = this;
+        const isFullscreen = figure.classList.contains('vpd-diagram--fullscreen');
+
+        document.querySelectorAll('.vpd-diagram').forEach(diagram => {
+          diagram.classList.remove('vpd-diagram--fullscreen');
+        });
+
+        if (!isFullscreen) {
+          figure.classList.add('vpd-diagram--fullscreen');
+        }
+      `},[t("img",{src:"/minipi_textbook/asset/diagrams/mermaid-basic-10-c3d65e909d98b97c22a570147ddeccac.svg",alt:"mermaid Diagram",class:"vpd-diagram-image"})],-1),s("",86)])])}const D=a(L,[["render",V]]);export{j as __pageData,D as default};
