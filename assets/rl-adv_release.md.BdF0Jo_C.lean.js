e23e45b185990d02aeb3f96b2751db74a0f658ee
sw.han <sw.han@wego-robotics.com>
2025-12-10 15:25:00 +0900

    init

---
+++ assets/rl-adv_release.md.BdF0Jo_C.lean.js
@@ -0,0 +1,12 @@
+import{_ as a,c as l,o as n,j as i,ah as t,a as h}from"./chunks/framework.B6i8j-aH.js";const g=JSON.parse('{"title":"Mini Pi 및 Mini Pi Plus에서 모션 AI 배포","description":"","frontmatter":{},"headers":[],"relativePath":"rl-adv/release.md","filePath":"rl-adv/release.md"}'),e={name:"rl-adv/release.md"};function p(k,s,r,o,d,c){return n(),l("div",null,[...s[0]||(s[0]=[i("h1",{id:"mini-pi-및-mini-pi-plus에서-모션-ai-배포",tabindex:"-1"},[h("Mini Pi 및 Mini Pi Plus에서 모션 AI 배포 "),i("a",{class:"header-anchor",href:"#mini-pi-및-mini-pi-plus에서-모션-ai-배포","aria-label":"Permalink to “Mini Pi 및 Mini Pi Plus에서 모션 AI 배포”"},"​")],-1),i("figure",{class:"vpd-diagram vpd-diagram--mermaid",onclick:`
+        const figure = this;
+        const isFullscreen = figure.classList.contains('vpd-diagram--fullscreen');
+
+        document.querySelectorAll('.vpd-diagram').forEach(diagram => {
+          diagram.classList.remove('vpd-diagram--fullscreen');
+        });
+
+        if (!isFullscreen) {
+          figure.classList.add('vpd-diagram--fullscreen');
+        }
+      `},[i("img",{src:"/minipi_textbook/asset/diagrams/mermaid-release-3-51e07d009b4c2b60b30384a36f2834c6.svg",alt:"mermaid Diagram",class:"vpd-diagram-image"})],-1),t("",33)])])}const y=a(e,[["render",p]]);export{g as __pageData,y as default};
